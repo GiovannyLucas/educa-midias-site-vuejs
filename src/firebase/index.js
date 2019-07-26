8426f11d4647637a413371b3f4a3da5cfa9115e1
LUIZDV1D <gamesbrdavi@gmail.com>
2019-07-26 14:51:26 -0300

    firebase data

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,13 +4,13 @@ import 'firebase/storage'
 import 'firebase/database'
 
 export const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyAeLSaUVJHx20UIrZZby1lGErCH2nsvJhc',
-  authDomain: 'site-educa-midias.firebaseapp.com',
-  databaseURL: 'https://site-educa-midias.firebaseio.com',
-  projectId: 'site-educa-midias',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
   storageBucket: '',
-  messagingSenderId: '22958374209',
-  appId: '1:22958374209:web:055dd942ce2f47d4'
+  messagingSenderId: '',
+  appId: ''
 
 })
 
