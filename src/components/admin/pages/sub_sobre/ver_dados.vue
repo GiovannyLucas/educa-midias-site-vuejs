d340c79ef03a26a16cee5a2c62d8c779c7627098
LUIZDV1D <gamesbrdavi@gmail.com>
2019-08-04 16:25:48 -0300

    updating 'ver dados sobre nós'

--- src/components/admin/pages/sub_sobre/ver_dados.vue
+++ src/components/admin/pages/sub_sobre/ver_dados.vue
@@ -1,40 +1,20 @@
 <template>
   <div class="container">
-  <h1> {{ dado.o_que_e }} </h1>
+  <h1> Ver dados sobre nós </h1>
     <v-card
       class="mx-auto"
       xs12
     >
       <v-img
         class="white--text"
-        height="200px"
+        style="height: 60vh;"
         :src="dado.img_dos_membros"
       >
-        <v-card-title class="align-end fill-height">Top 10 Australian beaches</v-card-title>
       </v-img>
 
       <v-card-text>
-        <span>Number 10</span><br>
-        <span class="text--primary">
-          <span>Whitehaven Beach</span><br>
-          <span>Whitsunday Island, Whitsunday Islands</span>
-        </span>
+        <span> {{ dado.o_que_e }} </span><br>
       </v-card-text>
-
-      <v-card-actions>
-        <v-btn
-          text
-          color="orange"
-        >
-          Share
-        </v-btn>
-        <v-btn
-          text
-          color="orange"
-        >
-          Explore
-        </v-btn>
-      </v-card-actions>
     </v-card>
   </div>
 </template>
